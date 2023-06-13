55991f72c206b1812e5c7daeaff37390edfda72d
AanshOjha <108977462+AanshOjha@users.noreply.github.com>
2023-06-13 19:18:30 +0530

    Shortest way to take INPUT, SPLIT by spaces, MAP FUNC. to convert to INTEGER, SORT in ascending order then printing in 1 in a line.

--- Beginner-Level/list.py
+++ Beginner-Level/list.py
@@ -1,3 +1,4 @@
+# Shortest way to take INPUT, SPLIT by spaces, MAP FUNC. to convert to INTEGER, SORT in ascending order then printing in 1 in a line.
 num = sorted(map(int, (input().split())))
 for i in num:
     print(i)
